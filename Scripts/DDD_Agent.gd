84de7136c07c19c134adc2cc5200819d72d6f448
FaetterP <56697273+FaetterP@users.noreply.github.com>
2025-01-16 21:09:00 +0300

    Переведён файл DDD_Agent

--- Scripts/DDD_Agent.gd
+++ Scripts/DDD_Agent.gd
@@ -20,14 +20,14 @@ const SFX_ADJUTS_LEVEL = 10
 var name_char = "DDD_Angent"
 var current_text = [""]
 var text_3312_spanish = ["Protocolo de limpieza completado.", "Puede continuar con su trabajo."]
-var text_3312_english = ["Cleaning protocol completed.", "You can continue your job."]
+var text_3312_english = ["Протокол очистки завершён.", "Вы можете продолжать свою работу."]
 var text_3312_chinese = ["清洁程序已完成。","您可以继续您的工作了。"]
 var text_tutorial_spanish = ["Bienvenido y felicidades por su nuevo empleo.", "Como pudo observar en el vídeo introductorio, su trabajo consiste en verificar el ingreso de los vecinos de su edificio.", "Cada día habrá una lista de individuos que solicitarán ingreso al edificio.", "Es posible que existan individuos que soliciten ingreso y no estén en la lista.", "En cuyo caso marcará en la lista de verificación que no se encuentran en la lista y procederá a interrogar al individuo.", "También debe verificar que la identificación y la solicitud de entrada sean correctos y con el respectivo logo del D.D.D.", "No olvide también revisar el vencimiento en las identificaciones (mm/aaaa).","Recuerde que es febrero de 1955.","Le deseo mucha suerte."]
-var text_tutorial_english = ["Welcome and congratulations on your new job.", "As you could see on the introductory film, your job is to verify the entry of the neighbors of your building.", "Each day there will be a list of individuals who will request entry to the building.", "It is possible that there are individuals who request entry and aren't on the list.", "In which case you will mark on the checklist that they are not on the list and proceed to question the individual.", "Also, you must verify that the ID and the entry request are correct and have the respective D.D.D. logo.", "Don't forget to also check the expiration on the IDs (mm/yyyy).","Remember it's February 1955.","I wish you good luck."]
+var text_tutorial_english = ["Добро пожаловать и поздравляем с вашей новой работой.", "Как вы могли увидеть в вводном фильме, ваша задача - проверять жителей вашего здания.", "Каждый день будет список людей, которые будут хотеть войти.", "Возможно, будут люди, которые хотят войти и не находятся в списке.", "В таком случае отметьте в контрольном списке, что их нет, и допросите этого человека.", "Кроме того, вы должны проверить, что удостоверение личности и разрешение на вход правильные и имеют соответствующий логотип D.D.D.", "Не забудьте также проверить срок действия удостоверений (мм/гггг).", "Помните, сейчас февраль 1955 года.", "Желаю вам удачи."]
 var text_tutorial_chinese = ["欢迎并祝贺您新工作就职。","如您在介绍视频中所见，您的工作是验证您大楼的居民入住情况。","每天都会有一个申请进入大楼的个人名单。","有可能会有一些人申请进入但并不在名单上。","在这种情况下，您将在检查清单上标注他们不在名单上，并继续询问该个体。","此外，您还必须验证身份证明文件和入学申请是否正确，并附有相应的D.D.D。标识。","不要忘记检查邻居身份证上的有效期。\n(月,年)","请记住，现在是1955年2月。","祝你好运。"]
 var is_nightmare = false
 var text_tutorial_nigthmare_spanish = ["Ya sabe que hacer."]
-var text_tutorial_nigthmare_english = ["You know the drill."]
+var text_tutorial_nigthmare_english = ["Вы знаете, что делать."]
 var text_tutorial_nigthmare_chinese = ["你知道该怎么做了。"]
 
 func _ready() -> void:
